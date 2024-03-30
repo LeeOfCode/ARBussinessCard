ae9fb49306f5c1fecea04546264267bb9998b7e3
Chris-LeirT <78292939+Chris-LeirT@users.noreply.github.com>
2024-03-30 22:39:13 +0700

    update model

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const THREE = window.MINDAR.IMAGE.THREE;
 import {CSS3DObject} from "./libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js";
+import {FBXLoader} from "./libs/three.js-r132/examples/jsm/loaders/FBXLoader.js";
+
+const loadFBX = (path) => {
+      return new Promise((resolve, reject) => {
+          const loader = new FBXLoader();
+          loader.load(path, (fbx) => {
+            resolve(fbx);
+          });
+      });
+  }
+
 document.addEventListener('DOMContentLoaded', () => {
   const startAsync = async() => {
 
@@ -8,6 +19,20 @@ document.addEventListener('DOMContentLoaded', () => {
       imageTargetSrc: './assets/bussinesscard.mind',
     });
     const {renderer, cssRenderer, scene, cssScene, camera} = mindarThree;
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    const model = await loadFBX("./assets/models/dancing.fbx")
+    model.scale.set(0.003,0.003,0.003);
+    model.position.set(0.7,-0.3,0);
+    // model.rotation.x = Math.PI/2;
+
+    const mixer = new THREE.AnimationMixer(model);
+    const action = mixer.clipAction(model.animations[0]);
+    action.play();
+
+    const modelAnchor = mindarThree.addAnchor(0);
+    modelAnchor.group.add(model);
 
     const obj = new CSS3DObject(document.querySelector("#my-card"));
     const cssAnchor = mindarThree.addCSSAnchor(0);
@@ -20,9 +45,13 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log("bye");
     }
 
+    const clock = new THREE.Clock();
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
+      const delta = clock.getDelta();
+      mixer.update(delta);
       cssRenderer.render(cssScene, camera);
+      renderer.render(scene, camera);
     });
   }
   startAsync();
